b6e178caf6b2b5b7d1d7bb07b130a63dd6466af3
Quentin Burny <burny.quentin@gmail.com>
2017-10-04 23:45:38 +0200

    Add Question component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,24 @@
 <template>
   <div id="app">
+    <ol class="list">
+      <li><Question></Question></li>
+      <li><Question></Question></li>
+      <li><Question></Question></li>
+    </ol>
+
   </div>
 </template>
 
 <script>
+import Question from './components/Question.vue';
+
 export default {
   name: 'app',
   data () {
     return {}
+  },
+  components: {
+  	Question
   }
 }
 </script>
@@ -17,7 +28,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
@@ -27,12 +37,11 @@ h1, h2 {
 }
 
 ul {
-  list-style-type: none;
+  list-style-type: circle;
   padding: 0;
 }
 
 li {
-  display: inline-block;
   margin: 0 10px;
 }
 
---
+++ src/components/Question.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="question">
+    Ceci est une question
+  </div>
+</template>
+<style>
+</style>
+<script>
+	export default {
+		props: {},
+		data() {
+			return {}
+		},
+		methods: {},
+		components: {}
+	}
+</script>
